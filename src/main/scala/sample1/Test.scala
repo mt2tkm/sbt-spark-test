c98734fb2f0cc9ff5ccab40dc78d2ab03a82d3e3
mt2tkm <sakaimatsumotot@gmail.com>
2017-06-07 17:47:54 +0900

    first commit

---
+++ src/main/scala/sample1/Test.scala
@@ -0,0 +1,17 @@
+package sbtsparktest.sample1
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+import org.canova.api.records.reader.impl.CSVRecordReader
+
+import org.nd4j.linalg.lossfunctions.LossFunctions
+import org.slf4j.LoggerFactory
+
+import scala.io.Source
+
+object test {
+  def main(args: Array[String]) {
+
+    println(11)
+  }
+}
