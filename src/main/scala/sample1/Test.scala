6f47a75c50364065994e5bec741d90f27f617539
mt2tkm <sakaimatsumotot@gmail.com>
2017-06-19 21:32:45 +0900

    ┌(｡△｡)┐

--- src/main/scala/sample1/Test.scala
+++ src/main/scala/sample1/Test.scala
@@ -5,6 +5,7 @@ import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 
+import scala.collection.mutable.HashMap
 import org.apache.spark.sql._
 
 import org.slf4j.LoggerFactory
@@ -31,14 +32,127 @@ object test {
 
     //仮想的なviewの作成
     testDF.registerTempTable("test")
-    val sql = "select _4,_5,_6,_7,_8,_13,_14,_15,_18,_19,_20,_21,_22,sum(_10),sum(_11),sum(_12) from test group by _4,_5,_6,_7,_8,_13,_14,_15,_18,_19,_20,_21,_22"
+    val sql = "select _4,_6,_7,_8,_13,_14,_15,_18,_19,_20,_21,_22,sum(_10) as _10,sum(_11) as _11,sum(_12) as _12 from test group by _4,_6,_7,_8,_13,_14,_15,_18,_19,_20,_21,_22"
     val test_view = sqlContext.sql(sql)
+    val name = Seq( "item", "aFlg", "bFlg", "device", "cFlg","sex", "age", "region", "color", "colorcate", "size", "bigcate", "addfee", "fee", "num" )
+    val view = test_view.toDF(name: _*)
 
-    //クエリ結果の作成
-
+    //クエリ結果の作成？
+    //SearchSD(view)
+    SeeDB(view)
 
     log.info("finished")
     println("================================================")
+  }
+
+  //SeeDB
+  private def SeeDB(df : DataFrame){
+      val map_a = scala.collection.mutable.HashMap.empty[String, scala.collection.mutable.HashMap[String,(Int,Int,Int)]]
+      val map_b = scala.collection.mutable.HashMap.empty[String, scala.collection.mutable.HashMap[String,(Int,Int,Int)]]
+      val col = df.columns
+
+      val A = (7,"大阪府")
+      val B = (7,"神奈川県")
+
+      //df.filter("region = '大阪府'").show
+      df.rdd.filter{row => row(A._1).toString == A._2 || row(B._1).toString == B._2}
+      .toLocalIterator.foreach{line =>
+          val addfee = line(12).toString.toInt
+          val fee = line(13).toString.toInt
+          val num = line(14).toString.toInt
+
+          if( line(A._1).toString == A._2 ){
+          // 条件Aの内容を含むもの
+              for(i<-0 until col.length-3){
+                  if(i != A._1){
+                      if(map_a contains col(i)){
+                          if(map_a(col(i)) contains line(i).toString){
+                              map_a(col(i))(line(i).toString) = ( map_a(col(i))(line(i).toString)._1 + addfee, map_a(col(i))(line(i).toString)._2 + fee, map_a(col(i))(line(i).toString)._3 + num)
+                          }
+                          else{
+                              map_a(col(i)) += line(i).toString -> (addfee, fee, num)
+                          }
+                      }
+                      else{
+                          map_a(col(i)) = HashMap(line(i).toString -> (addfee, fee, num))
+                      }
+                  }
+              }
+          }
+          else{
+          // 条件Bの内容を含むもの
+              for(i<-0 until col.length-3){
+                  if(i != B._1){
+                      if(map_b contains col(i)){
+                          if(map_b(col(i)) contains line(i).toString){
+                              map_b(col(i))(line(i).toString) = ( map_b(col(i))(line(i).toString)._1 + addfee, map_b(col(i))(line(i).toString)._2 + fee, map_b(col(i))(line(i).toString)._3 + num)
+                          }
+                          else{
+                              map_b(col(i)) += line(i).toString -> (addfee, fee, num)
+                          }
+                      }
+                      else{
+                          map_b(col(i)) = HashMap(line(i).toString -> (addfee, fee, num))
+                      }
+                  }
+              }
+          }
+      }
+      for(i<-map_a.keys){
+          println(i)
+          println(map_a(i))
+      }
+      println("-------------------------------------------")
+      for(i<-map_b.keys){
+          println(i)
+          println(map_b(i))
+      }
+
+  }
+
+  //Search subset of data
+  private def SearchSD(df : DataFrame){
+      val map = scala.collection.mutable.HashMap.empty[String, scala.collection.mutable.HashMap[String,(Int,Int,Int)]]
+
+      df.rdd.toLocalIterator.foreach{line =>
+          val addfee = line(12).toString.toInt
+          val fee = line(13).toString.toInt
+          val num = line(14).toString.toInt
+
+          //条件指定
+          val x_value = line(7).toString
+          val subsetdata = line(6).toString
+
+          //All data
+          if(map contains "all"){
+              if(map("all") contains x_value){
+                  map("all")(x_value) = ( map("all")(x_value)._1 + addfee, map("all")(x_value)._2 + fee, map("all")(x_value)._3 + num )
+              }
+              else{
+                  map("all") +=  x_value -> (addfee, fee, num)
+              }
+          }
+          else{
+              map("all") = HashMap(x_value -> (addfee, fee, num))
+          }
+
+          //subset data
+          if(map contains subsetdata){
+              if(map(subsetdata) contains x_value){
+                  map(subsetdata)(x_value) = ( map(subsetdata)(x_value)._1 + addfee, map(subsetdata)(x_value)._2 + fee, map(subsetdata)(x_value)._3 + num)
+              }
+              else{
+                  map(subsetdata) += x_value -> (addfee, fee, num)
+              }
+          }
+          else{
+              map(subsetdata) = HashMap(x_value -> (addfee, fee, num))
+          }
+      }
+      for(i<-map.keys){
+          println(i)
+          println(map(i))
+      }
   }
 
   //brand,bigcate,smallcateがうまくいかない
